4bc4e8cbb7607b65cc9ea071dbecfd592fdcd7de
Jesse Myrberg <jesse.myrberg@gmail.com>
2020-05-06 20:14:28 +0300

    Bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='finscraper',
-    version='0.0.1dev0',
+    version='0.0.1dev1',
     license='MIT',
     description='Web scraping API for Finnish websites',
     long_description=long_description,
