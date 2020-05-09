c4bfe7464b9575c930637db14536a94ef16e2af4
Jesse Myrberg <jesse.myrberg@gmail.com>
2020-05-09 22:23:32 +0300

    Scrape just 1 item for category scrapes in tests

--- tests/test_spiders.py
+++ tests/test_spiders.py
@@ -15,27 +15,27 @@ from finscraper.spiders import ISArticle, ILArticle
 
 def test_ISArticle_with_category():
     # Test scraping, no chromedriver
-    spider = ISArticle('ulkomaat').scrape(10)
+    spider = ISArticle('ulkomaat').scrape(1)
     df = spider.get()
-    assert len(df) >= 10
+    assert len(df) >= 1
     assert len(df.columns) == 8
 
     # Test scraping with chromedriver
-    spider = ISArticle('ulkomaat', allow_chromedriver=True).scrape(10)
+    spider = ISArticle('ulkomaat', allow_chromedriver=True).scrape(1)
     df = spider.get()
-    assert len(df) >= 10
+    assert len(df) >= 1
     assert len(df.columns) == 8
 
     # Test continuing scraping
-    df2 = spider.scrape(10).get()
-    assert len(df2) >= len(df) + 10
+    df2 = spider.scrape(1).get()
+    assert len(df2) >= len(df) + 1
 
     # Save and load spider
     jobdir = spider.save()
     spider = ISArticle.load(jobdir)
 
-    df3 = spider.scrape(10).get()
-    assert len(df3) >= len(df2) + 10
+    df3 = spider.scrape(1).get()
+    assert len(df3) >= len(df2) + 1
 
 
 def test_ISArticle_no_params():
@@ -59,21 +59,21 @@ def test_ISArticle_no_params():
 
 def test_ILArticle_with_category():
     # Test scraping
-    spider = ILArticle('ulkomaat').scrape(5)
+    spider = ILArticle('ulkomaat').scrape(1)
     df = spider.get()
-    assert len(df) >= 5
+    assert len(df) >= 1
     assert len(df.columns) == 8
 
     # Test continuing scraping
-    df2 = spider.scrape(10).get()
-    assert len(df2) >= len(df) + 10
+    df2 = spider.scrape(1).get()
+    assert len(df2) >= len(df) + 1
 
     # Save and load spider
     jobdir = spider.save()
     spider = ILArticle.load(jobdir)
 
-    df3 = spider.scrape(10).get()
-    assert len(df3) >= len(df2) + 10
+    df3 = spider.scrape(1).get()
+    assert len(df3) >= len(df2) + 1
 
 
 def test_ILArticle_no_params():
