d40a81c51e70aa38456b531a0aeabe6b8abe49fc
Jesse Myrberg <jesse.myrberg@gmail.com>
2020-05-09 21:52:52 +0300

    Bump version to 0.0.1dev4

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ copyright = '2020, Jesse Myrberg'
 author = 'Jesse Myrberg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1dev3'
+release = '0.0.1dev4'
 
 
 # -- General configuration ---------------------------------------------------
