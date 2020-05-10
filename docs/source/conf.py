af01a13b440c15ad59a4bb603e202e4ee48000a9
Jesse Myrberg <jesse.myrberg@gmail.com>
2020-05-10 12:56:54 +0300

    Version 0.0.01dev6 + travis update

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -46,7 +46,7 @@ copyright = '2020, Jesse Myrberg'
 author = 'Jesse Myrberg'
 
 # The full version, including alpha/beta/rc tags
-release = '0.0.1dev5'
+release = '0.0.1dev6'
 
 
 # -- General configuration ---------------------------------------------------
