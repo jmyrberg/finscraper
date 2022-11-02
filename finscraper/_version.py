ea7858d18935ae415e27b7dbb630d05a23b05b29
Jesse Myrberg <jesse.myrberg@gmail.com>
2022-11-02 23:09:57 +0200

    Use pep440-pre

--- finscraper/_version.py
+++ finscraper/_version.py
@@ -43,7 +43,7 @@ def get_config():
     # _version.py
     cfg = VersioneerConfig()
     cfg.VCS = "git"
-    cfg.style = "pep440"
+    cfg.style = "pep440-pre"
     cfg.tag_prefix = ""
     cfg.parentdir_prefix = "finscraper-"
     cfg.versionfile_source = "finscraper/_version.py"
