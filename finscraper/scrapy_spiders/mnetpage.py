3d1b05812ac5847d45aad6288c33fcdd22385795
Jesse Myrberg <jesse.myrberg@gmail.com>
2023-09-10 16:16:30 +0300

    Make MNetPageItem use FollowAndParse

--- finscraper/scrapy_spiders/mnetpage.py
+++ finscraper/scrapy_spiders/mnetpage.py
@@ -1,32 +1,43 @@
-from scrapy import Item, Field, Selector, Request
-from scrapy.linkextractors import LinkExtractor
-from scrapy.loader import ItemLoader
-from scrapy.spiders import Rule, CrawlSpider
-from scrapy.http.response.html import HtmlResponse
-from itemloaders.processors import TakeFirst, Identity, Compose
-from finscraper.text_utils import strip_join, strip_elements, drop_empty_elements
+"""Module for Muuskoiden.net forum spider."""
 
 import time
 
+from scrapy import Item, Field, Selector
+from scrapy.crawler import Spider
+from scrapy.linkextractors import LinkExtractor
+from scrapy.loader import ItemLoader
 
-class _MNetPageSpider(CrawlSpider):
-    name = "mnetpage"
-    start_urls = ["https://muusikoiden.net/keskustelu/conferences.php"]
+from finscraper.scrapy_spiders.mixins import FollowAndParseItemMixin
+from itemloaders.processors import TakeFirst, Identity, Compose
+from finscraper.text_utils import strip_join, strip_elements, \
+    drop_empty_elements
 
-    rules = [
-        Rule(LinkExtractor(allow=r"keskustelu/\d+", deny=r"posts.php?.+")),
-        Rule(LinkExtractor(allow=r"posts.php?.+"), callback="_parse_item"),
-    ]
+
+class _MNetPageSpider(FollowAndParseItemMixin, Spider):
+    name = 'mnetpage'
+    start_urls = ['https://muusikoiden.net/keskustelu/conferences.php']
+    follow_link_extractor = LinkExtractor(
+        allow_domains=('muusikoiden.net'),
+        allow=(r'keskustelu\/\d+'),
+        deny=(r'posts.php?.+'),
+        deny_domains=(),
+        canonicalize=True
+    )
+    item_link_extractor = LinkExtractor(
+        allow_domains=('muusikoiden.net'),
+        allow=(r'keskustelu\/posts.php?.+'),
+        deny=(),
+        deny_domains=(),
+        canonicalize=True
+    )
+    custom_settings = {}
 
     def __init__(self, *args, **kwargs):
-        """
-        Scrape content from "muusikoiden.net" web forum.
-        """
+        """Scrape content from 'muusikoiden.net' web forum."""
         super(_MNetPageSpider, self).__init__(*args, **kwargs)
-        self._base_url = "https://muusikoiden.net"
 
-    def _parse_message(self, message: Selector, author: str, time: str):
-        """Parse message on thread page
+    def _parse_message(self, message: Selector, author: str, time_posted: str):
+        """Parse message on thread page.
 
         Args:
             message (Selector): Selector for message
@@ -36,16 +47,16 @@ class _MNetPageSpider(CrawlSpider):
         Returns:
             ItemLoader: Item with data about the message
         """
-        il = ItemLoader(item=_MnetMessageItem(), selector=message)
-        il.add_value("author", author)
-        il.add_value("time_posted", time)
-        il.add_xpath("quotes", '//i[@class="quote"]//text()')
-        il.add_xpath("content", '//font[@class="msg"]/text()[not(self::i)]')
+        il = ItemLoader(item=_MNetMessageItem(), selector=message)
+        il.add_value('author', author)
+        il.add_value('time_posted', time_posted)
+        il.add_xpath('quotes', '//i[@class="quote"]//text()')
+        il.add_xpath('content', '//font[@class="msg"]/text()[not(self::i)]')
 
         return il.load_item()
 
     def _parse_item(self, resp):
-        """Parse thread page
+        """Parse thread page.
 
         Args:
             resp (HtmlResponse): Response from fetching the thread
@@ -54,11 +65,10 @@ class _MNetPageSpider(CrawlSpider):
             _MNetPageItem: _description_
         """
         il = ItemLoader(item=_MNetPageItem(), response=resp)
-        print(type(resp))
-        il.add_value("url", resp.url)
-        il.add_value("time", int(time.time()))
-        il.add_xpath("title", '//b[contains(@class, "linkcolor")]//text()')
-        il.add_xpath("page_number", '(//b[@class="selected_page"])[1]//text()')
+        il.add_value('url', resp.url)
+        il.add_value('time', int(time.time()))
+        il.add_xpath('title', '//b[contains(@class, "linkcolor")]//text()')
+        il.add_xpath('page_number', '(//b[@class="selected_page"])[1]//text()')
 
         messages = []
         message_authors = resp.xpath('//a[@class="keskustelu_nick"]//text()')
@@ -70,17 +80,17 @@ class _MNetPageSpider(CrawlSpider):
                 self._parse_message(
                     message=Selector(text=message.get()),
                     author=message_authors[i].get(),
-                    time=message_timestamps[i].get(),
+                    time_posted=message_timestamps[i].get(),
                 )
             )
 
         # add messages into thread item
-        il.add_value("messages", messages)
+        il.add_value('messages', messages)
 
         return il.load_item()
 
 
-class _MnetMessageItem(Item):
+class _MNetMessageItem(Item):
     """
     Returned message fields:
         * author (str): Author of the message.
@@ -88,7 +98,6 @@ class _MnetMessageItem(Item):
         * quotes (list of str): List of quotes in the message.
         * content (str): Contents of the message.
     """
-
     author = Field(
         input_processor=strip_join,
         output_processor=TakeFirst()
@@ -112,15 +121,14 @@ class _MnetMessageItem(Item):
 
 
 class _MNetPageItem(Item):
-    """
-    Returned thread page fields:
+    __doc__ = """
+    Returned page fields:
         * url (str): Url of the thread page.
         * time (int): UNIX timestamp of the scraping.
         * title (str): Title of the thread.
         * page_number (int): Number of the page in the thread.
         * messages (list of str): Messages on the thread page.
-    """
-
+    """ + _MNetMessageItem.__doc__
     url = Field(
         input_processor=Identity(),
         output_processor=TakeFirst()
